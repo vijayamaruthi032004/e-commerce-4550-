07aeda2b15940898eb9653e5796ca792d2087788
vijayamaruthi032004 <vijayamaruthi10@gmail.com>
2025-08-02 10:12:13 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,23 @@
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+
+    public Product(int id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public double getPrice() { return price; }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + name + " - ₹" + price + "\n" + description;
+    }
+}
